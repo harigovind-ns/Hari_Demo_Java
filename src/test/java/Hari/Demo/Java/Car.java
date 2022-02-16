66b015b1685432f64cc585627cb8dd1e612d7a83
harigovind-ns <harigovind.ns@gmail.com>
2022-02-15 21:36:56 -0600

    first commit

---
+++ src/test/java/Hari/Demo/Java/Car.java
@@ -0,0 +1,17 @@
+package Hari.Demo.Java;
+
+public class Car {
+
+    public static void main(String[] args){
+        Car crv= new Car();
+        crv.brake();
+
+
+    }
+
+
+    public void brake(){
+        System.out.println("barke applied");
+    }
+
+}
